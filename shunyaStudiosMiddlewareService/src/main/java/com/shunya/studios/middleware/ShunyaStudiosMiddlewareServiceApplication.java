f619788f6b1d2c782af70b1b2837beb955a404eb
Aryan Notiyal <aryannotiyal@Ryans-MAC.local>
2025-10-18 15:12:30 +0530

    Initial commit - Shunya Studios Middleware Service

---
+++ shunyaStudiosMiddlewareService/src/main/java/com/shunya/studios/middleware/ShunyaStudiosMiddlewareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shunya.studios.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShunyaStudiosMiddlewareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShunyaStudiosMiddlewareServiceApplication.class, args);
+	}
+
+}
---
+++ shunyaStudiosMiddlewareService/src/main/java/com/shunya/studios/middleware/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.shunya.studios.middleware.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Shunya Studios Middleware Service 🚀";
+    }
+}
